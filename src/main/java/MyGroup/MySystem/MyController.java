f7020daf1f036ec96b3a479a0e1939724f288394
yuiffy <yuiffy@qq.com>
2017-01-12 20:20:56 +0800

    first commit

---
+++ src/main/java/MyGroup/MySystem/MyController.java
@@ -0,0 +1,27 @@
+package MyGroup.MySystem;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MyController {
+    @RequestMapping("/")
+    String onePage() {
+        return "indexpage";
+    }
+
+    @RequestMapping("/body")
+    @ResponseBody
+    String bodyTest() {
+        return "responseBody!返回博迪！";
+    }
+
+    @RequestMapping("/testAction")
+    String testAction(@RequestParam("numA") int x, @RequestParam("numB") int y, Model model) {
+        model.addAttribute("numC", x + y);
+        return "onepage";
+    }
+}
---
+++ src/main/resources/static/js/test.js
@@ -0,0 +1,28 @@
+/**
+ * Created by yuiff on 2016/12/22.
+ */
+var table =
+    {
+        "data": {
+            "languages": [{"id": 3, "name": "DAN"}, {"id": 4, "name": "DEU"}, {"id": 5, "name": "ETI"}],
+            "lines": [
+                {
+                    "items": [
+                        {"translations": []},
+                        {
+                            "translations": [{
+                                "priority": 1,
+                                "projectInfoId": 16,
+                                "translateStringInfoId": 1,
+                                "translateString": "设置！"
+                            }]
+                        },
+                        {"translations": []}
+                    ],
+                    "engStringInfoId": 1,
+                    "engString": "setting"
+                }
+            ],
+            "projectInfoName": {"16": "MT51\/Source\/AU\/TCL"}
+        }, "msg": "success"
+    }
